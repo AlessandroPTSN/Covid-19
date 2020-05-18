a39b2de47aca890e8096816b6a4d20e5959d517b
Alessandro Pereira Torres De Sá Neto <alessandroptsn@yahoo.com.br>
2020-05-18 12:55:28 -0300

    Update ui.R

--- ui.R
+++ ui.R
@@ -14,7 +14,7 @@ ui <- navbarPage("Covid-19",theme = shinytheme("slate"),
                                                                 options = list(maxItems = 5, placeholder = 'Escolha os locais:'))))
                           ),
                           column(
-                            12,fluidRow(column(12, plotlyOutput('plot'))
+                            12,fluidRow(column(12, plotlyOutput('plot')%>% withSpinner(color="#0dc5c1"))
                             )
                           ),
                           mainPanel(
@@ -36,7 +36,7 @@ ui <- navbarPage("Covid-19",theme = shinytheme("slate"),
                                                                 options = list(maxItems = 5, placeholder = 'Escolha os locais:'))))
                           ),
                           column(
-                            12,fluidRow(column(12, plotlyOutput('plot2'))
+                            12,fluidRow(column(12, plotlyOutput('plot2')%>% withSpinner(color="#0dc5c1"))
                             )
                           ),
                           
