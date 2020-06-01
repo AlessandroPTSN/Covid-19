4b8316c81a0017273504ea2ec63d409fefbfa04e
Alessandro Pereira Torres De Sá Neto <alessandroptsn@yahoo.com.br>
2020-06-01 15:40:27 -0300

    Update server.R

--- server.R
+++ server.R
@@ -1,10 +1,37 @@
 # Server code
 server <- function(input, output) {
   
+  
+  outVarr <- reactive({
+    df11 %>%
+      filter(countriesAndTerritories %in% input$Alll) %>%
+      mutate(countriesAndTerritories = paste(countriesAndTerritories, "casos", sep = " ")) %>% 
+      arrange(Data) %>%
+      droplevels()
+  })
+  
+  outVarr2 <- reactive({
+    df22 %>%
+      filter(countriesAndTerritories %in% input$Alll2) %>%
+      mutate(countriesAndTerritories = paste(countriesAndTerritories, "mortes", sep = " ")) %>% 
+      arrange(Data) %>%
+      droplevels()
+  })
+
+  output$Total <- renderPlotly({
+    plot_ly(data=outVarr(), x=~Data,  y = ~Quantidade,
+            type = 'scatter', mode = 'lines', legendgroup = "1",
+            color = ~countriesAndTerritories,colors = "viridis") %>%
+      add_trace(data=outVarr2(), x=~Data,  y = ~deaths,
+                type = 'scatter', mode = 'lines', legendgroup = "2",
+                color = ~countriesAndTerritories,colors = "viridis")  %>%
+      layout(legend = list(orientation = 'h',y = 100, x = 0))
+  })
+  
   outVar <- reactive({
     df1 %>%
       filter(countriesAndTerritories %in% input$All) %>%
-      mutate(countriesAndTerritories = paste(countriesAndTerritories, "casos." ,"Total:",x,"Incidência:",incidencia, sep = " ")) %>% 
+      mutate(countriesAndTerritories = paste(countriesAndTerritories, "casos", sep = " ")) %>% 
       arrange(Data) %>%
       droplevels()
   })
@@ -12,28 +39,54 @@ server <- function(input, output) {
   outVar2 <- reactive({
     df2 %>%
       filter(countriesAndTerritories %in% input$All2) %>%
-      mutate(countriesAndTerritories = paste(countriesAndTerritories, "mortes.","Total:",x,"Mortalidade:",mortalidade, sep = " ")) %>% 
+      mutate(countriesAndTerritories = paste(countriesAndTerritories, "mortes", sep = " ")) %>% 
       arrange(Data) %>%
       droplevels()
   })
   
-  output$plot <- renderPlotly({
+  output$Incidencia <- renderPlotly({
     plot_ly(data=outVar(), x=~Data,  y = ~Quantidade,
             type = 'scatter', mode = 'lines', legendgroup = "1",
             color = ~countriesAndTerritories,colors = "viridis") %>%
       add_trace(data=outVar2(), x=~Data,  y = ~deaths,
                 type = 'scatter', mode = 'lines', legendgroup = "2",
                 color = ~countriesAndTerritories,colors = "viridis")  %>%
-      layout(legend = list(orientation = 'v'))
+      layout(legend = list(orientation = 'h',y = 100, x = 0))
   })
   
+  
+  outVarrr <- reactive({
+    df111 %>%
+      filter(countriesAndTerritories %in% input$Allll) %>%
+      mutate(countriesAndTerritories = paste(countriesAndTerritories, "casos", sep = " ")) %>% 
+      arrange(Data) %>%
+      droplevels()
+  })
+  
+  outVarrr2 <- reactive({
+    df222 %>%
+      filter(countriesAndTerritories %in% input$Allll2) %>%
+      mutate(countriesAndTerritories = paste(countriesAndTerritories, "mortes", sep = " ")) %>% 
+      arrange(Data) %>%
+      droplevels()
+  })
+  
+  output$Acumulado <- renderPlotly({
+    plot_ly(data=outVarrr(), x=~Data,  y = ~Quantidade,
+            type = 'scatter', mode = 'lines', legendgroup = "1",
+            color = ~countriesAndTerritories,colors = "viridis") %>%
+      add_trace(data=outVarrr2(), x=~Data,  y = ~deaths,
+                type = 'scatter', mode = 'lines', legendgroup = "2",
+                color = ~countriesAndTerritories,colors = "viridis")  %>%
+      layout(legend = list(orientation = 'h',y = 100, x = 0))
+  })
   
   
   
   outVara <- reactive({
     cidade %>%
       filter(countriesAndTerritories %in% input$All11) %>%
-      mutate(countriesAndTerritories = paste(countriesAndTerritories, "casos acumulados. Total:",x, sep = " ")) %>% 
+      mutate(countriesAndTerritories = paste(countriesAndTerritories, "casos acumulados", sep = " ")) %>% 
       arrange(Data) %>%
       droplevels()
   })
@@ -41,7 +94,7 @@ server <- function(input, output) {
   outVara2 <- reactive({
     cidade2 %>%
       filter(countriesAndTerritories %in% input$All22) %>%
-      mutate(countriesAndTerritories = paste(countriesAndTerritories, "mortes acumuladas. Total:",x, sep = " ")) %>% 
+      mutate(countriesAndTerritories = paste(countriesAndTerritories, "mortes acumuladas", sep = " ")) %>% 
       arrange(Data) %>%
       droplevels()
   })
@@ -53,8 +106,36 @@ server <- function(input, output) {
       add_trace(data=outVara2(), x=~Data,  y = ~deaths,
                 type = 'scatter', mode = 'lines', legendgroup = "2",
                 color = ~countriesAndTerritories,colors = "viridis")  %>%
-      layout(legend = list(orientation = 'v'))         
+      layout(legend = list(orientation = 'h',y = 100, x = 0))         
   })
+  
+  output$table <- DT::renderDataTable({
+    DT::datatable(tabela1,style = 'bootstrap',options = list(
+      initComplete = JS(
+        "function(settings, json) {
+        $(this.api().table().header()).css({
+        'background-color': '#000',
+        'color': '#fff'
+        }); 
+        }")
+    ))
+  })
+  
+  output$table2 <- DT::renderDataTable({
+    DT::datatable(tabela2,style = 'bootstrap',options = list(
+      initComplete = JS(
+        "function(settings, json) {
+        $(this.api().table().header()).css({
+        'background-color': '#000',
+        'color': '#fff'
+        }); 
+        }")
+
+    ))
+  })
+  
+
+  
 }
 # Return a Shiny app object
 #shinyApp(ui = ui, server = server)
