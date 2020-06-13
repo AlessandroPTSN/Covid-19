f996e93c7bc84b5b4f96ad0243c60eb7d94655cd
Marcus Nunes <marcus.nunes@gmail.com>
2020-06-13 10:54:26 -0300

    Funcao para baixar os dados

---
+++ baixarDados.R
@@ -0,0 +1,16 @@
+# funcao que baixa os dados nacionais e internacionais
+# caso os arquivos em disco tenham mais de 24 horas de idade
+
+# dados nacionais
+
+my_data <- read.csv("https://brasil.io/dataset/covid19/caso_full/?format=csv", na.strings = "", fileEncoding = "UTF-8-BOM")
+
+# dados internacionais
+
+df1 <- read.csv("https://opendata.ecdc.europa.eu/covid19/casedistribution/csv", na.strings = "", fileEncoding = "UTF-8-BOM")
+
+# salva os objetos em um arquivo .rda, 
+# cuja leitura eh mais rapida do que .csv
+
+save(list = c("my_data", "df1"), file = "dados.rda")
+
