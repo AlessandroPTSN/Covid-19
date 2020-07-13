dd0332e86fbc4d99901e69564031f155663215ff
Alessandro Pereira Torres De Sá Neto <alessandroptsn@yahoo.com.br>
2020-07-13 14:30:53 -0300

    dados nacionais atualizados

--- baixarDados.R
+++ baixarDados.R
@@ -4,7 +4,20 @@
 # dados nacionais
 
 #my_data <- read.csv("https://brasil.io/dataset/covid19/caso_full/?format=csv", na.strings = "", fileEncoding = "UTF-8-BOM")
-my_data <- read.csv("https://raw.githubusercontent.com/AlessandroPTSN/Covid-19/master/my_data2.csv")
+#my_data <- read.csv("https://raw.githubusercontent.com/AlessandroPTSN/Covid-19/master/my_data2.csv")
+download_brasilio_table <- function(dataset, table_name){
+  url <- sprintf("https://data.brasil.io/dataset/%s/%s.csv.gz", dataset, table_name)
+  tmp <- tempfile()
+  download.file(url, tmp)
+  response <- read.csv(gzfile(tmp), encoding = "UTF-8")
+  unlink(tmp)
+  return(response)
+}
+
+# Passe o nome da tabela para a funcao, como "caso", "caso_full", "obito_cartorio":
+my_data <- download_brasilio_table("covid19", "caso_full")
+my_data = my_data %>%  filter(city!="")
+
 
 # dados internacionais
 
